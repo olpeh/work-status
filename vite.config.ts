3046c47183c55a960057baa2669144d99984d73a
Olavi Haapala <mail@olavihaapala.fi>
2024-01-11 23:00:16 +0200

    Init github action and

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "work-status",
+    lib: {
+      entry: "src/main.tsx",
+      name: "work-status",
+      fileName: "index",
+    },
+  },
 });
