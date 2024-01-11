05aca284e30a5c9a3edaea1871021cfe42c482b7
Johannes Gangsö <johannes.gangso@avenla.fi>
2024-01-12 00:46:04 +0200

    Fix Wordpress endpoint to return valid JSON

--- work-status.php
+++ work-status.php
@@ -28,8 +28,8 @@ add_shortcode( 'work_report', function () {
 	ob_start();
 	?>
 
-    <link rel="stylesheet" href="<?php echo plugins_url( __DIR__ . '/index.css' ); ?>"/>
-    <script type="module" src="<?php echo plugins_url( __DIR__ . '/index.js' ); ?>"></script>
+    <link rel="stylesheet" href="<?php echo plugins_url( 'index.css', __FILE__ ); ?>"/>
+    <script type="module" src="<?php echo plugins_url( 'index.js', __FILE__ ); ?>"></script>
 
     <div id="work-root"></div>
 
@@ -37,7 +37,6 @@ add_shortcode( 'work_report', function () {
 	return ob_get_clean();
 
 } );
-
 
 /**
  * Provide endpoint for fetching WORK data
@@ -47,66 +46,72 @@ add_action( 'wp_ajax_fetch_work_reports', function () {
 	$reports = [];
 
 	if ( have_rows( 'reports', 'option' ) ) {
-		while ( have_rows( 'repeater_field_name', 'option' ) ) {
+		while ( have_rows( 'reports', 'option' ) ) {
 			the_row();
 
-            $data = convert_work_report_data( get_sub_field( 'data' ) );
+			$data = convert_work_report_data( get_sub_field( 'data' ), get_sub_field( 'title' ) );
 
-			$reports[ sanitize_title( get_sub_field( 'name' ) ) ] = $data;
+			$reports[] = $data;
 		}
 	}
+
+	echo json_encode( $reports, JSON_PRETTY_PRINT );
+
+	wp_die();
 
 } );
 
 
 /**
  * @param string $data Tab separated values copied from reporting Excel
+ * @param string $title Name of the report
  *
- * @return string
+ * @return array
  */
-function convert_work_report_data( $data ): string {
+function convert_work_report_data( string $data, string $title ): array {
 
-    // Split the input into lines
-    $lines = explode("\n", $data);
+	// Split the input into lines
+	$lines = explode( "\n", $data );
 
-    // Parse the header line to get the keys
-    $headers = explode("\t", array_shift($lines));
-    $jsonObject = [
-        "DateFrom" => "",
-        "DateTo" => "",
-        "MyShareTargetOnDate" => 0,
-        "MyShareNextTreshold" => 0,
-        "SamvirkGoalPerMonth" => 0,
-        "Teams" => []
-    ];
+	// Parse the header line to get the keys
+	$headers = explode( "\t", array_shift( $lines ) );
+	$arr     = [
+		"Title"                => $title,
+		"DateFrom"            => "",
+		"DateTo"              => "",
+		"MyShareTargetOnDate" => 0,
+		"MyShareNextTreshold" => 0,
+		"SamvirkGoalPerMonth" => 0,
+		"Teams"               => []
+	];
 
-    foreach ($lines as $line) {
-        $rowData = explode("\t", $line);
-        $teamData = array_combine($headers, $rowData);
-        $jsonObject["DateFrom"] = date("Y-m-d", strtotime($teamData["DateFrom"]));
-        $jsonObject["DateTo"] = date("Y-m-d", strtotime($teamData["DateTo"]));
-        $jsonObject["MyShareTargetOnDate"] = intval($teamData["MyShareTargetOnDate"]);
-        $jsonObject["MyShareNextTreshold"] = intval($teamData["MyShareNextTreshold"]);
-        $jsonObject["SamvirkGoalPerMonth"] = intval($teamData["SamvirkGoalPerMonth"]);
+	foreach ( $lines as $line ) {
+		$rowData                    = explode( "\t", $line );
+		$teamData                   = array_combine( $headers, $rowData );
+		$arr["DateFrom"]            = date( "Y-m-d", strtotime( $teamData["DateFrom"] ) );
+		$arr["DateTo"]              = date( "Y-m-d", strtotime( $teamData["DateTo"] ) );
+		$arr["MyShareTargetOnDate"] = intval( $teamData["MyShareTargetOnDate"] );
+		$arr["MyShareNextTreshold"] = intval( $teamData["MyShareNextTreshold"] );
+		$arr["SamvirkGoalPerMonth"] = intval( $teamData["SamvirkGoalPerMonth"] );
 
-        $team = [
-            "Name" => $teamData["TeamName"],
-            "MemberCount" => intval($teamData["MemberCount"]),
-            "BUKStatus" => [
-                "MemberCount" => intval($teamData["BUKMemberCount"]),
-                "OnTrackCount" => intval($teamData["BUKOnTrackCount"])
-            ],
-            "SamvirkStatus" => [
-                "MemberCount" => intval($teamData["SamvirkMemberCount"]),
-                "ContributionAmount" => intval($teamData["SamvirkContributionAmount"])
-            ],
-            "AHStatus" => [
-                "ContributionAmount" => intval($teamData["AHContributionAmount"])
-            ]
-        ];
-        $jsonObject["Teams"][] = $team;
-    }
+		$team           = [
+			"Name"          => $teamData["TeamName"],
+			"MemberCount"   => intval( $teamData["MemberCount"] ),
+			"BUKStatus"     => [
+				"MemberCount"  => intval( $teamData["BUKMemberCount"] ),
+				"OnTrackCount" => intval( $teamData["BUKOnTrackCount"] )
+			],
+			"SamvirkStatus" => [
+				"MemberCount"        => intval( $teamData["SamvirkMemberCount"] ),
+				"ContributionAmount" => intval( $teamData["SamvirkContributionAmount"] )
+			],
+			"AHStatus"      => [
+				"ContributionAmount" => intval( $teamData["AHContributionAmount"] )
+			]
+		];
+		$arr["Teams"][] = $team;
+	}
 
-    return json_encode($jsonObject, JSON_PRETTY_PRINT);
+	return $arr;
 
 }
