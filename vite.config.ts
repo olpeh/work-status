928c761bab8edc5618023e53d37df70fc07fde36
Olavi Haapala <mail@olavihaapala.fi>
2024-01-12 23:38:20 +0200

    Try modifying the output

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "work-status",
     lib: {
       entry: "src/main.tsx",
       name: "work-status",
