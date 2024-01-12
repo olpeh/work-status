457ebd9a7499a4d498152d2cecadfc227822703c
Olavi Haapala <mail@olavihaapala.fi>
2024-01-13 00:00:23 +0200

    Try to define process env

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    lib: {
-      entry: "src/main.tsx",
-      name: "work-status",
-      fileName: "index",
+export default defineConfig((mode) => {
+  return {
+    define: {
+      "process.env.NODE_ENV": JSON.stringify(mode),
     },
-  },
+    plugins: [react()],
+    build: {
+      lib: {
+        entry: "src/main.tsx",
+        name: "work-status",
+        fileName: "index",
+      },
+    },
+  };
 });
